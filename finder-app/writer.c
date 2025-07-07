5d74867c39d88303da133cf0ee45dfa86ceb9d85
artb83 <21041434+artb83@users.noreply.github.com>
2025-07-07 15:56:53 +0300

    Addapt Makefile for cross-compiling. In writer.c open syslog once, close if error happens and exit program or at the end of the program.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,17 +5,9 @@
 #include <string.h>
 
 
-/*
- * writer.c
- *
- *  Created on: Jun 11, 2025
- *      Author: artb
- */
-
-
 int main(int argc, const char** argv){
+	openlog(NULL, 0, LOG_USER);
 	if( argc!=3 ){
-		openlog(NULL, 0, LOG_USER);
 		syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
 		closelog();
 		return 1;
@@ -24,13 +16,11 @@ int main(int argc, const char** argv){
 	const char* strToWrite = argv[2];
 	int fd = open(pathToFile, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP );
 	if(fd<0){
-		openlog(NULL, 0, LOG_USER);
 		syslog(LOG_ERR, "Could not open file: %s", pathToFile);
 		closelog();
 		return 1;
 	}
 	write(fd, strToWrite, strlen(strToWrite));
-	openlog(NULL, 0, LOG_USER);
 	syslog(LOG_DEBUG, "Writing %s to file %s", strToWrite, pathToFile);
 	closelog();
 	close(fd);
